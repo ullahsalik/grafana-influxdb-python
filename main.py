b3cd27ccfef5fb39f9d2488ac78eeb5f9e57e2b3
Salik Ullah <ullahsalik@gmail.com>
2020-07-02 09:03:10 +0530

    added logs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, time
 import psutil, socket
 from influxdb import InfluxDBClient
 from influxdb.client import InfluxDBClientError
@@ -11,19 +11,51 @@ db_host = parser.get('db_credentials', 'db_host')
 db_port = parser.get('db_credentials', 'db_port')
 USER = parser.get('db_credentials', 'db_user')
 PASSWORD = parser.get('db_credentials', 'db_passwd')
-DBNAME = parser.get('db_credentials', 'db_name')
+DBNAME = parser.get('db_config', 'db_name')
+LOG_DBNAME = parser.get('db_config', 'log_db_name')
+LOG_PATH = parser.get('db_config', 'log_file_path')
 
 
 def main(host=db_host, port=db_port):
-    client = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
-    print("Create database: " + DBNAME)
+
+    client_1 = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
+    client_2 = InfluxDBClient(host, port, USER, PASSWORD, LOG_DBNAME)
+    print("Check for database: " + DBNAME + ", " + LOG_DBNAME)
     try:
-        client.create_database(DBNAME)
+        client_1.create_database(DBNAME)
+        client_2.create_database(LOG_DBNAME)
     except InfluxDBClientError as e:
+        print(e)
+
+    try:
+        print("Checking for the log file: " + LOG_PATH)
+        f = open(LOG_PATH, 'r')
+        log_file_name = LOG_PATH.split('/')[-1]
+    except Exception as e:
         print(e)
 
     metric = socket.gethostname()
     while True:
+        line = ''
+        while len(line) == 0 or line[-1] != '\n':
+            tail = f.readline()
+            if tail == '':
+                time.sleep(0.1)          # avoid busy waiting
+                # f.seek(0, io.SEEK_CUR) # appears to be unneccessary
+                continue
+            line += tail
+
+        point_log_value = [{
+            "measurement": "{}-{}".format(metric, log_file_name),
+            "fields": {
+                "log": line,
+            },
+            "tags": {
+                "hostName": socket.gethostname(),
+            },
+        }]
+        client_2.write_points(point_log_value)
+
         pointValues = [{
             "measurement": metric,
             "fields": {
@@ -38,7 +70,8 @@ def main(host=db_host, port=db_port):
                 "hostName": socket.gethostname(),
             },
         }]
-        client.write_points(pointValues)
+        client_1.write_points(pointValues)
+
 
 def parse_args():
     """Parse the args."""
